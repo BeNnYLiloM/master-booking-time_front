05d9be1aaab0a356f0bc7626900adc580bdfe180
Roman Ismailov <benny-krid@mail.ru>
2026-02-06 11:42:04 +0300

    пытаюсь запустить отзывы из сообщения

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { debugHelper } from '../utils/debugHelper'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
